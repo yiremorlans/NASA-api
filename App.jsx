9a1486863a072f9dd9ef64df9139ef90df0812a5
Yire Morlans <yire.m@icloud.com>
2022-11-14 22:21:09 -0500

    fetch picture of day with image info

--- App.jsx
+++ App.jsx
@@ -1,14 +1,24 @@
-export default class App extends React.Component{
+
+export default class App extends React.Component {
+
+    state = { date: '', imageInfo: {} }
+
+    fetchPOD = () => {
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        .then(res => res.json())
+        .then(data => this.setState({ imageInfo: data }))
+  }
+
     render () {
         return (
             <>
                 <h1>Travel back to a time</h1>
-                <input type="date" name="date" value=""/>
-                <button type="button" name="button">Blast off</button>
+                <input type="date" name="date" value={this.state.date} onChange={event => this.setState({ date: event.currentTarget.value })}/>
+                <button type="button" name="button" onClick={this.fetchPOD}>Blast off</button>
 
-                <img src="" alt="" />
-                <h2>Deep in space...</h2>
-                <p></p>
+                <img src={this.state.imageInfo.url} alt="" />
+                <h2>{this.state.imageInfo.title}</h2>
+                <p>{this.state.imageInfo.explanation}</p>
             </>
         )
     }
