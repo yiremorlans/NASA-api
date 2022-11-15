54193b6370bcaa2894cbf891ab4757176f78aa7a
Yire Morlans <yire.m@icloud.com>
2022-11-15 14:59:18 -0500

    refactor fetch as react component

--- App.jsx
+++ App.jsx
@@ -1,25 +1,35 @@
+// import {config} from './js/config'
 
-export default class App extends React.Component {
 
-    state = { date: '', imageInfo: {} }
+// console.log(config)
+export default function App() {
+    const [date, setDate] = React.useState('')
+    const [imageInfo, setImageInfo] = React.useState({})
 
-    fetchPOD = () => {
-        fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+    const fetchPOD = () => {
+        const isDateInFuture = new Date(date) > new Date()
+        if (isDateInFuture){
+            return setImageInfo({
+                title: 'The future has not been captured yet..', 
+                url: 'https://www.science.org/do/10.1126/science.aaa7852/full/sn-interstellarh.jpg', 
+                explanation: '' 
+            })
+        }
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
         .then(res => res.json())
-        .then(data => this.setState({ imageInfo: data }))
+        .then(data => setImageInfo(data))
+        
   }
 
-    render () {
-        return (
-            <>
-                <h1>Travel back to a time</h1>
-                <input type="date" name="date" value={this.state.date} onChange={event => this.setState({ date: event.currentTarget.value })}/>
-                <button type="button" name="button" onClick={this.fetchPOD}>Blast off</button>
 
-                <img src={this.state.imageInfo.url} alt="" />
-                <h2>{this.state.imageInfo.title}</h2>
-                <p>{this.state.imageInfo.explanation}</p>
-            </>
-        )
-    }
+    return (
+        <>
+            <h1>Travel back to a time</h1>
+            <input type="date" name="date" value={date} onChange={event => setDate(event.currentTarget.value)}/>
+            <button type="button" name="button" onClick={fetchPOD}>Blast off</button>
+            <img src={imageInfo.url} alt="" />
+            <h2>{imageInfo.title}</h2>
+            <p>{imageInfo.explanation}</p>
+        </>
+    )
 }
