39d27946e087ea5ae195d732a1eedd2cce816e75
Yire Morlans <yire.m@icloud.com>
2022-11-14 19:48:52 -0500

    setup react App

---
+++ App.jsx
@@ -0,0 +1,15 @@
+export default class App extends React.Component{
+    render () {
+        return (
+            <>
+                <h1>Travel back to a time</h1>
+                <input type="date" name="date" value=""/>
+                <button type="button" name="button">Blast off</button>
+
+                <img src="" alt="" />
+                <h2>Deep in space...</h2>
+                <p></p>
+            </>
+        )
+    }
+}
